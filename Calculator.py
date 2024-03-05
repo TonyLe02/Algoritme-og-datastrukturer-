5a46e72859f59f1882d7ef66a42836d8d0264d6a
Tony Le <111498490+TonyLe02@users.noreply.github.com>
2024-03-05 12:09:01 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+#CALCULATOR
+num1 = float(input("Enter first number: "))
+op = input("Enter operator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*"or op == "x":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
+
+# Run the code
